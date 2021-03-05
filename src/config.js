8fcfb18d63fb3832a8ee4bdb3da3c0cdd1b47ccf
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2021-03-04 22:14:05 -0600

    fixed login/signup page glitch

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config =  {
     API_ENDPOINT: 'https://envelope-finance-server.herokuapp.com',
-    TOKEN_KEY: ''
+    TOKEN_KEY: 'e231a2a7-78f1-481e'
     //Token KEY on remote API server
   }
 
